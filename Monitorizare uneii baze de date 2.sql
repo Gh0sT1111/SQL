359633be94563e8bf84f7d001d014d948c5c942f
Gh0sT1111 <116391320+Gh0sT1111@users.noreply.github.com>
2025-09-05 08:07:38 +0300

    Update Monitorizare uneii baze de date 2.sql

--- Monitorizare uneii baze de date 2.sql
+++ Monitorizare uneii baze de date 2.sql
@@ -1,8 +1,8 @@
-SELECT relname, --Numele tabelului (rel = relation) spre exemplu la noi este angajati,concedii etc..
-       seq_scan,--Câte scanări secvențiale au fost făcute (fără index, lent)
-       idx_scan,--Câte scanări prin index au fost făcute (mai rapid)
-       n_tup_ins,--	Câte rânduri au fost inserate în acel tabel
-       n_tup_upd,--	Câte rânduri au fost modificate (UPDATE)
-       n_tup_del--	Câte rânduri au fost șterse (DELETE)
+SELECT relname, 
+       seq_scan,
+       idx_scan,
+       n_tup_ins,
+       n_tup_upd,
+       n_tup_del
 FROM pg_stat_user_tables
 ORDER BY seq_scan DESC;
